221f12d8c0e8dd24b05a0b98e5de218422137dbf
Roman <thehand@yandex.ru>
2023-06-04 22:14:16 +0300

    Cart persist mechanism made

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store, { persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,46 @@
-import { configureStore } from '@reduxjs/toolkit';
-
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth-slice';
 import { cartItemsReducer } from './cart-items-slice';
 import { shopItemsReducer } from './shop-items-slice';
 import { uiReducer } from './ui-slice';
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    cart: cartItemsReducer,
-    shop: shopItemsReducer,
-    ui: uiReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  shop: shopItemsReducer,
+  ui: uiReducer,
+  cart: cartItemsReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export default store;
 
